9f1db5bb0d2dbbaa77737884a140c470f6161428
makarow_andrew <makarovaoa2rambler.ru@mail.ru>
2023-10-19 18:07:58 +0400

    Upgrade algorithm_selection.sh

--- Bash/algorithm_selection.sh
+++ Bash/algorithm_selection.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-while : 
-do
+algorithm_selection(){
 	echo "Введите число для выбора сложности алгоритма"
 	echo "1) O(n**2)"
 	echo "2) O(n * log(n))"
 	
 	read var_1
 	
-	echo "Вы ввели цифру: $var_1. Уверены в выборе сложности алгоритма?"
-	echo "Введите да/нет"
-	
-	read var_2
-	
-	if [ "$var_2" == "да" ]; then
-		echo "Запускаем скрипт"
-		break
+	if [[ "$var_1" =~ ^[0-9]+$ ]]; then
+		echo "Вы ввели число: $var_1"
+		if [ "$var_1" == "1" ]; then
+			echo "Сортировка пузырьком"
+			cd ../Python
+			python3 bubble_sort.py
+		elif [ "$var_1" == "2" ]; then
+			echo "Быстрая сортировка"
+			cd ../Python
+			python3 quick_sort.py
+		else
+			echo "Такого числа нет в списке"
+			algorithm_selection
+		fi
+	else
+		echo "Необходимо ввести число"
+		algorithm_selection
 	fi
-done
-
+	
+}
+algorithm_selection 
